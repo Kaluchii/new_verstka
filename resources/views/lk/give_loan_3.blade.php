bf4ddb2708834f4b3b4b3e9157134859bd554f9a
kaluchii <mr.kaluchii@mail.ru>
2018-07-26 16:47:17 +0600

    Start card page

--- resources/views/lk/give_loan_3.blade.php
+++ resources/views/lk/give_loan_3.blade.php
@@ -29,7 +29,7 @@
                         <div class="obtaining-methods__clarification">В РАБОЧЕЕ ВРЕМЯ</div>
                     </li>
                     <li class="obtaining-methods__item">
-                        <button class="obtaining-methods__check-button check-button"><span class="check-button__text">В отделении Казпочты</span></button>
+                        <button class="obtaining-methods__check-button check-button"><span class="check-button__text">В отделении «Казпочты»</span></button>
                         <div class="obtaining-methods__clarification">В РАБОЧЕЕ ВРЕМЯ</div>
                     </li>
                 </ul>
@@ -37,14 +37,25 @@
             <div class="personal-area__tabs obtaining-method-tabs">
                 <div class="obtaining-method-tabs__tab">
                     <div class="obtaining-method-tabs__title">На банковскую карту</div>
+
+                    <div class="obtaining-method-tabs__processing-img-wrap">
+                        <img src="#" alt="" class="obtaining-method-tabs__processing-img">
+                    </div>
+                    <p class="obtaining-method-tabs__data-protect"> Реквизиты Вашей карты используются только во время операции и передаются в Visa/MasterCard по защищенному каналу. Данные Вашей карты хранятся в зашифрованном виде и не могут быть переданы третьему лицу.</p>
+                    <ul class="obtaining-method-tabs__protects-list">
+                        <li class="obtaining-method-tabs__protects-item">
+                            <img src="#" alt="" class="obtaining-method-tabs__protect-logo">
+                        </li>
+                    </ul>
                 </div>
+
                 <div class="obtaining-method-tabs__tab">
                     <div class="obtaining-method-tabs__title">На счет в банке</div>
                     <p class="obtaining-method-tabs__clarification">Указанный счет должен быть открыт на имя заявителя. В случае одобрения, мы отправим деньги в течение 15 минут и сообщим по SMS.</p>
                     <div class="obtaining-method-tabs__2-cols">
                         <div class="obtaining-method-tabs__col">
                             <div class="obtaining-method-tabs__field obtaining-method-tabs__field--iban field">
-                                <div class="field__name">Ваш 20-ти значный IBAN <span class="normal">(номер счета)</span></div>
+                                <div class="field__name">Ваш 20-ти значный IBAN <span class="normal tablet-hide">(номер счета)</span></div>
                                 <div class="field__input-wrap">
                                     <input type="text" class="field__input input" placeholder="KZ00 0000 0000 0000 0000" tabindex="">
                                     <div class="field__alert"></div>
@@ -74,7 +85,41 @@
                         </div>
                     </div>
                     <div class="obtaining-method-tabs__btn-wrap">
-                        <button class="obtaining-method-tabs__btn button button--yellow button--big-in-small">Завершить оформление</button>
+                        <button class="obtaining-method-tabs__btn button button--yellow button--big-in-small"><span class="button__text">Завершить оформление</span></button>
+                    </div>
+                </div>
+
+                <div class="obtaining-method-tabs__tab is-active">
+                    <div class="obtaining-method-tabs__title">В отделении «Казпочты»</div>
+                    <p class="obtaining-method-tabs__clarification">Указанный счет должен быть открыт на имя заявителя. В случае одобрения, мы отправим деньги в течение 15 минут и сообщим по SMS. Получить займ можно в любом отделении «Казпочты» по всему Казахстану.</p>
+                    <div class="obtaining-method-tabs__2-cols">
+                        <div class="obtaining-method-tabs__col">
+                            <div class="obtaining-method-tabs__field obtaining-method-tabs__field--iban field">
+                                <div class="field__name">Ваш 20-ти значный IBAN <span class="normal tablet-hide">(номер счета)</span></div>
+                                <div class="field__input-wrap">
+                                    <input type="text" class="field__input input" placeholder="KZ00 0000 0000 0000 0000" tabindex="">
+                                    <div class="field__alert"></div>
+                                </div>
+                            </div>
+                            <div class="obtaining-method-tabs__popup-link-wrap">
+                                <a href="#" class="obtaining-method-tabs__popup-link link-local">КАК ОТКРЫТЬ СЧЕТ В «КАЗПОЧТЕ»</a>
+                            </div>
+                        </div>
+                        <div class="obtaining-method-tabs__col">
+                            <p class="obtaining-method-tabs__info-title">IBAN-номер счета в Казпочте можно узнать:</p>
+                            <div class="obtaining-method-tabs__iban-info text-block text-block--big-lh">
+                                <ol>
+                                    <li>В контакт-центре по номеру 1499;</li>
+                                    <li>Номер счета указан в договоре;</li>
+                                    <li>В отделении «Казпочты».</li>
+                                </ol>
+                                <p><span class="bold">Телефоны</span></p>
+                                <p>С городского +7 (727) 259-07-77</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="obtaining-method-tabs__btn-wrap">
+                        <button class="obtaining-method-tabs__btn button button--yellow button--big-in-small"><span class="button__text">Завершить оформление</span></button>
                     </div>
                 </div>
             </div>
