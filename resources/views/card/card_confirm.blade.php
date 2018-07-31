7a914b8ae4c3330f21f66af1b821af5f44f6d900
kaluchii <mr.kaluchii@mail.ru>
2018-07-31 16:54:40 +0600

    Card in process

--- resources/views/card/card_confirm.blade.php
+++ resources/views/card/card_confirm.blade.php
@@ -15,7 +15,44 @@
         <div class="card__surface">
             <div class="card__row">
                 <div class="card__row-name">Номер карты</div>
+                <div class="card__fields-wrap">
+                    <input type="text" autocomplete="off" class="card__field" maxlength="4" id="number_1">
+                    <input type="text" autocomplete="off" class="card__field" maxlength="4" id="number_2">
+                    <input type="text" autocomplete="off" class="card__field" maxlength="4" id="number_3">
+                    <input type="text" autocomplete="off" class="card__field" maxlength="4" id="number_4">
+                </div>
+            </div>
+            <div class="card__row">
+                <div class="card__row-name">Срок действия карты <span class="card__thru">VALID THRU</span></div>
+                <div class="card__fields-wrap card__fields-wrap--term">
+                    <select autocomplete="off" class="card__select" id="exp_month" name="exp_month">
+                        <option>01</option><option>02</option><option>03</option><option>04</option><option>05</option><option>06</option>
+                        <option>07</option><option>08</option><option>09</option><option>10</option><option>11</option><option>12</option>
+                    </select>
+                    <select autocomplete="off" class="card__select" id="exp_year" name="exp_year">
+                        <option>2016</option><option>2017</option><option>2018</option><option>2019</option><option>2020</option><option>2021</option><option>2022</option>
+                    </select>
+                </div>
+            </div>
+            <div class="card__row">
+                <div class="card__row-name">Имя на карте</div>
+                <div class="card__fields-wrap">
+                    <input autocomplete="off" type="text" class="card__field card__field--big" id="card_name" />
+                    <div class="card__field-error-hint">Имя держателя латинскими буквами</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="processing__front-side card">
+        <div class="card__side-name">Задняя сторона</div>
+        <div class="card__surface">
+            <div class="card__row">
+                <div class="card__row-name">Код на задней стороне <span class="">CVV2 CVC2</span></div>
+                <div class="card__fields-wrap">
+                    <input autocomplete="off" type="password" class="card__field" id="sec_cvv2" maxlength="3" />
+                    <div class="card__field-error-hint">Номер указан неверно</div>
+                </div>
             </div>
         </div>
     </div>
