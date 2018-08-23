0bd6edf6fe62e475fee233a79d7197a153489009
kaluchii <mr.kaluchii@mail.ru>
2018-08-23 14:21:30 +0600

    Changes

--- resources/views/lk/prolongation_wait.blade.php
+++ resources/views/lk/prolongation_wait.blade.php
@@ -35,18 +35,6 @@
                     </div>
                 </div>
             </div>
-            <div class="personal-area__btn-with-descr btn-with-descr">
-                <div class="btn-with-descr__btn-wrap">
-                    <a href="#" class="btn-with-descr__btn button button--yellow button--big-in-small"><span class="button__text">Погасить займ</span></a>
-                </div>
-                <p class="btn-with-descr__text">Оплата банковской картой или через терминалы оплат</p>
-            </div>
-            <div class="personal-area__btn-with-descr btn-with-descr">
-                <div class="btn-with-descr__btn-wrap">
-                    <a href="#" class="btn-with-descr__btn button button--yellow button--big-in-small"><span class="button__text">Продлить займ</span></a>
-                </div>
-                <p class="btn-with-descr__text">Продление до 30 дней доступно, пока займ не просрочен.</p>
-            </div>
             <div class="personal-area__info-block info-block">
                 <div class="info-block__top">
                     <p class="info-block__title title-l2">Информация по займу</p>
@@ -57,41 +45,34 @@
                         <div class="data-list__item-value">11 июля 2017</div>
                     </div>
                     <div class="data-list__item data-list__item--big">
-                        <div class="data-list__item-name">Погашенная сумма</div>
-                        <div class="data-list__item-value">18 570 тг</div>
+                        <div class="data-list__item-name">Комиссия за организацию и обслуживание Займа</div>
+                        <div class="data-list__item-value">570 тг</div>
                     </div>
                     <div class="data-list__item data-list__item--first-elements">
                         <div class="data-list__item-name">Сумма займа</div>
                         <div class="data-list__item-value">10 000 тг</div>
                     </div>
                     <div class="data-list__item data-list__item--big">
-                        <div class="data-list__item-name">Сумма к погашению сегодня</div>
-                        <div class="data-list__item-value">15 200 тг</div>
+                        <div class="data-list__item-name">Погашенная сумма</div>
+                        <div class="data-list__item-value">18 570 тг</div>
                     </div>
                     <div class="data-list__item data-list__item--first-elements">
                         <div class="data-list__item-name">Срок займа</div>
                         <div class="data-list__item-value">16 дней</div>
                     </div>
                     <div class="data-list__item data-list__item--big">
-                        <div class="data-list__item-name">К погашению в конце срока</div>
-                        <div class="data-list__item-value">25 860 тг</div>
+                        <div class="data-list__item-name">Сумма к погашению сегодня</div>
+                        <div class="data-list__item-value">15 200 тг</div>
                     </div>
                     <div class="data-list__item data-list__item--first-elements">
                         <div class="data-list__item-name">Дата возврата</div>
                         <div class="data-list__item-value">15 августа 2017</div>
                     </div>
+                    <div class="data-list__item data-list__item--big">
+                        <div class="data-list__item-name">К погашению в конце срока</div>
+                        <div class="data-list__item-value">25 860 тг</div>
+                    </div>
                 </div>
-            </div>
-            <div class="personal-area__info-block info-block">
-                <div class="info-block__top">
-                    <p class="info-block__title title-l2">Документация</p>
-                </div>
-                <ul class="info-block__row-list row-list">
-                    <li class="row-list__item"><a href="#" class="row-list__link link">Договор займа</a></li>
-                    <li class="row-list__item"><a href="#" class="row-list__link link">Дополнительное соглашение на продление займа от 01.01.2018</a></li>
-                    <li class="row-list__item"><a href="#" class="row-list__link link">Дополнительное соглашение на продление займа от 05.01.2018</a></li>
-                    <li class="row-list__item"><a href="#" class="row-list__link link">Дополнительное соглашение на реструктуризацию займа от 30.01.2018</a></li>
-                </ul>
             </div>
         </div>
     </div>
