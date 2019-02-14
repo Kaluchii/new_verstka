fbcd168390af7391246c78008304c7c8b6685dce
kaluchii <mr.kaluchii@mail.ru>
2019-02-14 16:47:28 +0600

    Changes

--- public/source/scripts/calculator.js
+++ public/source/scripts/calculator.js
@@ -1,22 +1,26 @@
 $(function () {
-    const amountSlider = document.getElementsByClassName('.js_amount_slider'),
-        termSlider = document.getElementsByClassName('.js_term_slider');
+    const amountSlider = document.getElementsByClassName('js_amount_slider')[0],
+        termSlider = document.getElementsByClassName('js_term_slider')[0];
 
     noUiSlider.create(amountSlider, {
-        start: [20, 80],
+        start: 20000,
+        step: 1000,
         connect: true,
+        tooltips: true,
         range: {
-            'min': 0,
-            'max': 100
+            'min': 7000,
+            'max': 150000
         }
     });
 
     noUiSlider.create(termSlider, {
-        start: [20, 80],
+        start: 10,
+        step: 1,
         connect: true,
+        tooltips: true,
         range: {
-            'min': 0,
-            'max': 100
+            'min': 5,
+            'max': 30
         }
     });
 });
