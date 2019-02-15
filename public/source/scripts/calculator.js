0bd075eee2a1ffdd38e065b9f8d69506cd14f386
kaluchii <mr.kaluchii@mail.ru>
2019-02-15 16:42:19 +0600

    Calculator in process

--- public/source/scripts/calculator.js
+++ public/source/scripts/calculator.js
@@ -1,26 +1,78 @@
 $(function () {
-    const amountSlider = document.getElementsByClassName('js_amount_slider')[0],
-        termSlider = document.getElementsByClassName('js_term_slider')[0];
+    const amountSlider = $('.js_amount_slider'),
+        termSlider = $('.js_term_slider');
 
-    noUiSlider.create(amountSlider, {
-        start: 20000,
-        step: 1000,
-        connect: true,
-        tooltips: true,
-        range: {
-            'min': 7000,
-            'max': 150000
+    amountSlider.slider({
+        range  : "min",
+        min    : 7000,
+        max    : 150000,
+        value  : 20000,
+        step   : 1000,
+        animate: false,
+        /*slide  : function (e, ui) {
+            changeSliderAmount(ui.value);
+        },
+        change : function (e, ui) {
+            changeSliderAmount(ui.value);
+        },
+        start  : function (e, ui) {
+            $(e.target).closest('.row').addClass('active');
+        },
+        stop   : function (e, ui) {
+            $(e.target).closest('.row').removeClass('active');
+        }*/
+        slide  : function (e, ui) {
+            amountSlider.find('.ui-slider-handle').attr('data-amount', `${ui.value} тг`)
+        },
+        change : function (e, ui) {
+            amountSlider.find('.ui-slider-handle').attr('data-amount', `${ui.value} тг`)
+        },
+        start  : function (e, ui) {
+        },
+        stop   : function (e, ui) {
         }
     });
 
-    noUiSlider.create(termSlider, {
-        start: 10,
-        step: 1,
-        connect: true,
-        tooltips: true,
-        range: {
-            'min': 5,
-            'max': 30
-        }
+    termSlider.slider({
+        range  : "min",
+        min    : 5,
+        max    : 30,
+        value  : 10,
+        step   : 1,
+        animate: false,
+        /*slide  : function (e, ui) {
+            changeSliderDays(ui.value);
+        },
+        change : function (e, ui) {
+            changeSliderDays(ui.value);
+        },
+        start  : function (e, ui) {
+            $(e.target).closest('.row').addClass('active');
+        },
+        stop   : function (e, ui) {
+            $(e.target).closest('.row').removeClass('active');
+        }*/
     });
+
+
+    $('.js_discount_item').on('click', function () {
+        // var currentDiscount = regular_discounts[$(this).data('discountIndex')],
+        //     term = currentDiscount.max_days,
+        //     amount = parseInt($('input[name="amount"]').val().replace(' ', ''), 10);
+
+        // cancelPromoCode();
+
+        // $("#promocode").val(currentDiscount.promocode);
+        // promoInfo = currentDiscount;
+        // $('#slider_days').slider('value', term);
+
+        $(this).siblings().removeClass('is-active');
+        $(this).addClass('is-active');
+        $('.js_discount_cancel').parent().removeClass('hide');
+        // $('.js_loan_discount').text(currentDiscount.percent * 100);
+        // $('.js_loan_term').text(term + ' ' + declension(term, ['день', 'дня', 'дней']));
+        // $('.js_loan_sum').html(formatPrice(amount, 's', true));
+        $('.js_discount_about').slideDown(300);
+    });
+
 });
--- resources/views/lk/calculate.blade.php
+++ resources/views/lk/calculate.blade.php
@@ -24,7 +24,7 @@
                             </div>
                             <div class="row-slider__value-wrap">
                                 <input type="text" class="row-slider__input input" value="20 000">
-                                <span class="row-slider__value-unit">тг</span>
+                                <span class="row-slider__value-unit">ТГ</span>
                             </div>
                         </div>
                         <div class="calculator__sliders-list-item row-slider">
@@ -36,7 +36,7 @@
                             </div>
                             <div class="row-slider__value-wrap">
                                 <input type="text" class="row-slider__input input" value="15">
-                                <span class="row-slider__value-unit">дней</span>
+                                <span class="row-slider__value-unit">ДНЕЙ</span>
                             </div>
                         </div>
                     </div>
@@ -51,33 +51,49 @@
                         </div>
                     </div>
                     <div class="calculator__result-row">
-                        <div class="calculator__info">
-                            <div class="calculator__info-col">
-                                <div class="calculator__info-col-title">Берете</div>
-                                <div class="calculator__info-col-value">20&nbsp;000&nbsp;тг</div>
+                        <div class="calculator__result-row-container">
+                            <div class="calculator__info">
+                                <div class="calculator__info-col">
+                                    <div class="calculator__info-col-title">Берете</div>
+                                    <div class="calculator__info-col-value">20&nbsp;000&nbsp;тг</div>
+                                </div>
+                                <div class="calculator__info-col">
+                                    <div class="calculator__info-col-title">Вернете</div>
+                                    <div class="calculator__info-col-value">22&nbsp;000&nbsp;тг</div>
+                                    <div class="calculator__info-col-old-value">23&nbsp;000&nbsp;тг</div>
+                                </div>
+                                <div class="calculator__info-col">
+                                    <div class="calculator__info-col-title">Возврат&nbsp;до</div>
+                                    <div class="calculator__info-col-value">20&nbsp;декабря</div>
+                                </div>
                             </div>
-                            <div class="calculator__info-col">
-                                <div class="calculator__info-col-title">Вернете</div>
-                                <div class="calculator__info-col-value">22&nbsp;000&nbsp;тг</div>
-                                <div class="calculator__info-col-old-value">23&nbsp;000&nbsp;тг</div>
+                            <div class="calculator__give-btn-wrap">
+                                <a href="#" class="calculator__give-btn button button--t-yellow button--calculate button--s-full-on-small">
+                                    <span class="button__text">Получить деньги</span>
+                                </a>
                             </div>
-                            <div class="calculator__info-col">
-                                <div class="calculator__info-col-title">Возврат&nbsp;до</div>
-                                <div class="calculator__info-col-value">20&nbsp;декабря</div>
-                            </div>
-                        </div>
-                        <div class="calculator__give-btn-wrap">
-                            <a href="#" class="calculator__give-btn button button--t-yellow button--s-full-on-small">
-                                <span class="button__text">Получить деньги</span>
-                            </a>
                         </div>
                     </div>
                     <div class="calculator__promocode promocode">
                         <div class="promocode__field-wrap">
-                            <div class="promocode__where-get"></div>
-                            <div class="promocode__input-wrap">
-                                <input type="text" class="promocode__input input" placeholder="Есть промо-код?">
-                                <span class="promocode__status">Код устарел</span>
+                            <div class="promocode__where-get where-promocode">
+                                <div class="where-promocode__container">
+                                    <div class="where-promocode__text-wrap">
+                                        <p class="where-promocode__text">Промо-код на скидки мы отправляем по почте и размещаем в соцсетях</p>
+                                        <div class="where-promocode__socials">
+                                            <a href="https://www.instagram.com/dopo.kz/" target="_blank" class="where-promocode__social-item where-promocode__social-item--inst"></a>
+                                            <a href="https://www.facebook.com/dopoluchki/" target="_blank" class="where-promocode__social-item where-promocode__social-item--fb"></a>
+                                            <a href="https://vk.com/dopo_kz" target="_blank" class="where-promocode__social-item where-promocode__social-item--vk"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p class="where-promocode__link link">Где узнать промо-код?</p>
+                            </div>
+                            <div class="promocode__input-wrap field js_field">
+                                <label class="field__input-wrap js_field_wrapper">
+                                    <input type="text" class="field__input input js_field_input" placeholder="Есть промо-код?">
+                                    <span class="field__alert field__alert--in-field js_field_alert">Код устарел</span>
+                                </label>
                             </div>
                             <div class="promocode__activate-btn-wrap">
                                 <button type="button" class="promocode__activate-btn button button--t-yellow">Применить</button>
@@ -87,6 +103,46 @@
                             <div class="promocode__info">Промо-код применен, скидка 21%</div>
                             <div class="promocode__cancel-wrap">
                                 <span class="promocode__cancel link">Отменить промокод</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="calculator__discounts discounts">
+                        <div class="discounts__container">
+                            <p class="discounts__title">Постоянные скидки <span class="no-wrap">для займа <span class="js_loan_sum">20 000 тг</span></span></p>
+                            <ul class="discounts__list">
+                                <li class="discounts__item js_discount_item" data-discount-index="DISC1">
+                                    <div class="discounts__item-container">
+                                        <div class="discounts__item-percent"><span class="mobile-show">Скидка </span>10%</div>
+                                        <div class="discounts__term"><span class="mobile-hide">Займ </span>на 21 день</div>
+                                    </div>
+                                </li>
+                                <li class="discounts__item js_discount_item" data-discount-index="DISC2">
+                                    <div class="discounts__item-container">
+                                        <div class="discounts__item-percent"><span class="mobile-show">Скидка </span>20%</div>
+                                        <div class="discounts__term"><span class="mobile-hide">Займ </span>на 25 дней</div>
+                                    </div>
+                                </li>
+                                <li class="discounts__item js_discount_item" data-discount-index="DISC3">
+                                    <div class="discounts__item-container">
+                                        <div class="discounts__item-percent"><span class="mobile-show">Скидка </span>25%</div>
+                                        <div class="discounts__term"><span class="mobile-hide">Займ </span>на 30 дней</div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="discounts__discount-cancel-wrap hide">
+                                <span class="discounts__discount-cancel link js_discount_cancel">Отключить скидку</span>
+                            </div>
+                            <div class="discounts__info-wrap js_discount_about" style="display: none;">
+                                <div class="discounts__info">
+                                    <p class="discounts__info-title">Займ <span class="js_loan_sum">20 000 тг</span>
+                                        на <span class="js_loan_term">25 дней</span> <span class="no-wrap">со скидкой <span class="js_loan_discount">15</span>%</span>
+                                    </p>
+                                    <ul class="discounts__info-list check-list">
+                                        <li class="check-list__item">Скидка действует только для повторных клиентов;</li>
+                                        <li class="check-list__item">Скидка доступна только для займа сроком на <span class="js_loan_term">25 дней</span>;</li>
+                                        <li class="check-list__item">Сумма переплаты <span class="js_overpayment"></span> рассчитывается единовременно и не снижается при досрочном погашении.</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
